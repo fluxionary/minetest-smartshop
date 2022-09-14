973e638c4f22e3ae768edb9c48cf8e885fd93647
flux <25628292+fluxionary@users.noreply.github.com>
2022-09-14 14:32:38 -0700

    disable currency changing until #42 is fixed

--- settings.lua
+++ settings.lua
@@ -9,7 +9,8 @@ smartshop.settings = {
     storage_max_distance = tonumber(settings:get("smartshop.storage_max_distance")) or 30,
 	storage_link_time = tonumber(settings:get("smartshop.storage_link_time")) or 30,
 
-    change_currency = settings:get_bool("smartshop.change_currency", true),
+    -- disable until https://github.com/fluxionary/minetest-smartshop/issues/42 is fixed
+    --change_currency = settings:get_bool("smartshop.change_currency", true),
 	enable_refund = settings:get_bool("smartshop.enable_refund", true),
 
 	admin_shop_priv = settings:get("smartshop.admin_shop_priv") or "smartshop_admin",
